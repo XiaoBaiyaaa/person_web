02c6f62c3035f22e0c351bd62f18cf157c8e88c9
xiejun <1728527167@qq.com>
2021-12-08 23:08:53 +0800

    feat: 完成spring mvc框架的搭建

---
+++ src/main/java/com/xiejun/project/controller/hello_world/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.xiejun.project.controller.hello_world;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/test")
+public class HelloWorldController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String returnIndex() {
+        return "index";
+    }
+
+}
